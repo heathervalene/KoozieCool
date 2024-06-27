ed3c5024fea92113e5e39dd3df63ee679365ee4f
HeatherRouke <heather214@gmail.com>
2024-06-27 15:58:56 -0400

    style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <div className="page-footer">
+        <div className="page-footer footer">
             <div className="container">
                 <div className="row">
                     <div className="col l3 m6 s6">
@@ -13,9 +13,21 @@ const Footer = () => {
                         </div>
                         <div className="col l3 m6 s6">
                         <ul>
-                            <li>Facebook</li>
-                            <li>Instagram</li>
-                            <li>Twitter</li>
+                        <li>
+                <a className="social-link" href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-facebook-f"></i> Facebook
+                </a>
+              </li>
+              <li>
+                <a className="social-link" href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-instagram"></i> Instagram
+                </a>
+              </li>
+              <li>
+                <a className="social-link" href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-twitter"></i> Twitter
+                </a>
+              </li>
                             </ul>
                             </div>
                             </div>
