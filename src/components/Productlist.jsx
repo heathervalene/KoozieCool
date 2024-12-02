18a613aefe37dd1df107e9e254c3740fb8c66605
HeatherRouke <heather214@gmail.com>
2024-12-02 12:24:21 -0500

    fix image url

--- src/components/Productlist.jsx
+++ src/components/Productlist.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ProductSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -8,23 +8,23 @@ const ProductSlider = () => {
     {
       image: "/crop_top.png",
       title: "Lasagna Del Rey Crop Top",
-      link: "https://www.etsy.com/shop/Kooziecool"
+      link: "https://www.etsy.com/shop/Kooziecool",
     },
     {
       image: "/koozie.png",
       title: "Urethra Franklin Koozie",
-      link: "https://www.etsy.com/shop/Kooziecool"
+      link: "https://www.etsy.com/shop/Kooziecool",
     },
     {
       image: "/trucker_hat.png",
       title: "Billie Eyelash Trucker Hat",
-      link: "https://www.etsy.com/shop/Kooziecool"
+      link: "https://www.etsy.com/shop/Kooziecool",
     },
     {
       image: "/Barnard_tank.png",
       title: "Unisex Barnard Tank",
-      link: "https://www.etsy.com/shop/Kooziecool"
-    }
+      link: "https://www.etsy.com/shop/Kooziecool",
+    },
   ];
 
   useEffect(() => {
@@ -33,67 +33,84 @@ const ProductSlider = () => {
     };
 
     handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const nextProducts = () => {
-    setCurrentIndex((prevIndex) => 
+    setCurrentIndex((prevIndex) =>
       (prevIndex + productsPerView) % products.length
     );
   };
 
   const prevProducts = () => {
-    setCurrentIndex((prevIndex) => 
-      prevIndex === 0 ? products.length - productsPerView : prevIndex - productsPerView
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0
+        ? products.length - productsPerView
+        : prevIndex - productsPerView
     );
   };
 
-  const visibleProducts = products.slice(currentIndex, currentIndex + productsPerView);
+  const visibleProducts = products.slice(
+    currentIndex,
+    currentIndex + productsPerView
+  );
   if (visibleProducts.length < productsPerView) {
-    visibleProducts.push(...products.slice(0, productsPerView - visibleProducts.length));
+    visibleProducts.push(
+      ...products.slice(0, productsPerView - visibleProducts.length)
+    );
   }
 
   return (
     <div className="product-slider-container">
       <h3 className="featured-title">Featured Items</h3>
       <div className="product-slider">
-        <button 
-          className="slider-nav prev" 
+        <button
+          className="slider-nav prev"
           onClick={prevProducts}
           aria-label="Previous Products"
         >
           &#10094;
         </button>
-        
+
         <div className="product-display">
-          {visibleProducts.map((product, index) => (
-            <div 
-              key={product.title}
-              className="product"
-            >
-              <img 
-                src={product.image} 
-                alt={product.title} 
-                className="product-image" 
-              />
+          {visibleProducts.map((product) => (
+            <div key={product.title} className="product">
+              <a
+                href={product.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="product-image"
+                />
+              </a>
               <h5 className="product-title">{product.title}</h5>
-              <a 
-                href={product.link} 
+              <a
+                href={product.link}
                 className="buy"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"  className="heart-icon">
-                  <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/>
+                <svg
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  className="heart-icon"
+                >
+                  <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181" />
                 </svg>
               </a>
             </div>
           ))}
         </div>
-        
-        <button 
-          className="slider-nav next" 
+
+        <button
+          className="slider-nav next"
           onClick={nextProducts}
           aria-label="Next Products"
         >
@@ -105,6 +122,7 @@ const ProductSlider = () => {
 };
 
 export default ProductSlider;
+
 
 const styles = `
 .product-slider-container {
